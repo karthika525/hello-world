f9ee8bd87b2106ff050b4f2740c841811f002ea4
karthika525 <karthikaindrajith@gmail.com>
2025-10-13 23:53:31 +0530

    A basic “Welcome Card” web page using React and Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import image from "./images/reactlogo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const userName = "Karthika";
+  console.log("React app started");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5 d-flex justify-content-center">
+    <div className="card shadow text-center" style={{ width: '28rem' }}>
+    <div className="card-body">
+    <h1 style={{ color: "#a60da1ff", fontWeight: "bold" }}> Welcome to React Learning, {userName}</h1>
+    <img src={image} alt="React Logo" className="img-fluid mb-3" style={{ width: "200px" }}/>
+    <img src="https://jelvix.com/wp-content/uploads/2024/09/react-js-examples.png" alt="React Example" className="img-fluid mb-3"/>
+    <p className='bg-primary' style={{ color: "#f4f9fbff" ,fontWeight: "light"}}>This is your first card with images and styles!</p>
+    </div>
+    </div>
     </div>
   );
-}
+};
 
 export default App;
